5f1630f8f1174ddae34bdaab81d6eecb89a79aa4
Chris Worcester <chrisworc@gmail.com>
2021-03-17 14:20:56 -0400

    141: Adjust camera angle and movement speeds

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -23,8 +23,12 @@ private:
 
 	FVector MoveDirection;
 	FQuat RotationDirection;
-	float MoveSpeed = 100.f;
-	float RotateSpeed = 100.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float MoveSpeed = 500.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float RotateSpeed = 250.f;
 
 	void CalculateMoveInput(float Value);
 	void CalculateRotateInput(float Value);
