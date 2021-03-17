442327dfb510965ac563e7becf11ebb8cf631bca
Chris Worcester <chrisworc@gmail.com>
2021-03-17 19:01:06 -0400

    142: Create turret class with function-calling timer

---
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APawnTurret::CheckFireCondition()
+{
+
+	UE_LOG(LogTemp, Warning, TEXT("Fire condition checked"));
+}
