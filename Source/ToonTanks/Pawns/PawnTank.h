2adf4297d3b7f9794d02b00bff08bfd7dfaab582
Chris Worcester <chrisworc@gmail.com>
2021-03-17 13:59:07 -0400

    140: Add movement controls to tank pawn

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -21,6 +21,16 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* Camera;
 
+	FVector MoveDirection;
+	FQuat RotationDirection;
+	float MoveSpeed = 100.f;
+	float RotateSpeed = 100.f;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRotateInput(float Value);
+	void Move();
+	void Rotate();
+
 public:
 	APawnTank();
 
