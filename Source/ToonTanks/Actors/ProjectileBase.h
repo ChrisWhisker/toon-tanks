029d34627c0259fb3ee5fedaac96e3d2824c58eb
Chris Worcester <chrisworc@gmail.com>
2021-03-24 10:00:02 -0400

    Add explanatory comments

--- Source/ToonTanks/Actors/ProjectileBase.h
+++ Source/ToonTanks/Actors/ProjectileBase.h
@@ -14,17 +14,28 @@ class TOONTANKS_API AProjectileBase : public AActor
 	GENERATED_BODY()
 
 private:
+	// VisibleAnywhere means it is read-only in all property windows
+	// BlueprintReadOnly means the variable is available as read-only within the blueprint event graph
+	// meta = (AllowPrivateAccess = "true") allows private variables to be accessible to blueprints
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UProjectileMovementComponent* ProjectileMovement;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* ProjectileMesh;
+
+	// EditDefaultsOnly means it's editable only on blueprints (not instances of the blueprint).
 	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	TSubclassOf<UDamageType> DamageType;
+
+	// EditAnywhere means it can be edited in propoerty windows, blueprints, and instances
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float MovementSpeed = 1300.f;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
 	float Damage = 50;
 
+	// UFUNCTION Exposes the function to Unreal
+	// TODO is this overriding???
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse,
 		const FHitResult& Hit);
